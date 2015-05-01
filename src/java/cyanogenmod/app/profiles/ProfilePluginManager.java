2699ed2a6d82e978551436af856e94749f13f664
Matt Garnes <matt@cyngn.com>
2015-05-01 14:25:14 -0700

    Add ProfilePluginManager.
    Change-Id: I0b53bc55f37e95a15062b15569da76241a20aeb3

---
+++ src/java/cyanogenmod/app/profiles/ProfilePluginManager.java
@@ -0,0 +1,59 @@
+package cyanogenmod.app.profiles;
+
+import android.content.Context;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import cyanogenmod.app.CMContextConstants;
+import cyanogenmod.app.profiles.IProfilePluginService;
+
+import java.util.List;
+
+public class ProfilePluginManager {
+    private static IProfilePluginService sService;
+    private static ProfilePluginManager sProfilePluginManagerInstance;
+    private Context mContext;
+
+    public ProfilePluginManager(Context context) {
+        Context appContext = context.getApplicationContext();
+        if (appContext != null) {
+            mContext = appContext;
+        } else {
+            mContext = context;
+        }
+        sService = getService();
+    }
+
+    /**
+     * Get or create an instance of the {@link cyanogenmod.app.ProfilePluginManager}
+     * @param context
+     * @return {@link cyanogenmod.app.profiles.ProfilePluginManager}
+     */
+    public static ProfilePluginManager getInstance(Context context) {
+        if (sProfilePluginManagerInstance == null) {
+            sProfilePluginManagerInstance = new ProfilePluginManager(context);
+        }
+        return sProfilePluginManagerInstance;
+    }
+
+    public void registerTrigger(String trigger, List<String> states) {
+        if (sService == null) {
+            sService = getService();
+        }
+        try {
+            sService.registerTrigger(trigger, states);
+        } catch (RemoteException e) {
+            //Unable to get service, fail
+        }
+    }
+
+    /** @hide */
+    public IProfilePluginService getService() {
+        if (sService != null) {
+            return sService;
+        }
+        IBinder b = ServiceManager.getService(CMContextConstants.CM_PROFILE_PLUGIN_SERVICE);
+        sService = IProfilePluginService.Stub.asInterface(b);
+        return sService;
+    }
+}
