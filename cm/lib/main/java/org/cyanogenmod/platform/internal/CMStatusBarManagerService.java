635f8f165775fb913ed3e7203f3801ac5cb60420
Jorge Ruesga <jorge@ruesga.com>
2015-06-14 00:07:09 +0200

    cmsdk: notify new listeners all registered custom tiles
    If a new listener is created, then notified all custom tiles that was previously registered
    
    Change-Id: I9e8d9a1fa84d54b00ea7fd8ed400e172f17f06cc
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- cm/lib/main/java/org/cyanogenmod/platform/internal/CMStatusBarManagerService.java
+++ cm/lib/main/java/org/cyanogenmod/platform/internal/CMStatusBarManagerService.java
@@ -113,6 +113,17 @@ public class CMStatusBarManagerService extends SystemService {
                                      final ComponentName component, final int userid) {
             enforceBindCustomTileListener();
             mCustomTileListeners.registerService(listener, component, userid);
+
+            // Notify registered tiles
+            try {
+                for (ExternalQuickSettingsRecord qsTile : mQSTileList) {
+                    if (userid == UserHandle.USER_ALL || qsTile.getUserId() == userid) {
+                        listener.onCustomTilePosted(new StatusBarCustomTileHolder(qsTile.sbTile));
+                    }
+                }
+            } catch (RemoteException re) {
+                // Ignore
+            }
         }
 
         /**
