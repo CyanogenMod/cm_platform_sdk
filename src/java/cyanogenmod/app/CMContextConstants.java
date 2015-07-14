0cdb1d513c70a794db0e29696ef620c573aa96ea
Matt Garnes <matt@cyngn.com>
2015-07-14 16:29:56 -0700

    Add SettingsManager.
    Add new APIs for changing a subset of system settings.
    
    Protected by cyanogenmod.permission.MODIFY_NETWORK_SETTINGS:
        - Add ability to toggle airplane mode on/off.
        - Add ability to toggle mobile data on/off.
    
    Protected by android.permission.REBOOT:
        - Add ability to shutdown or reboot the device.
    
    Change-Id: I5e943be11260c58afa664f1702c0ecb4413528fe

--- src/java/cyanogenmod/app/CMContextConstants.java
+++ src/java/cyanogenmod/app/CMContextConstants.java
@@ -50,4 +50,15 @@ public final class CMContextConstants {
      * @hide
      */
     public static final String CM_PROFILE_SERVICE = "profile";
+
+    /**
+     * Use with {@link android.content.Context#getSystemService} to retrieve a
+     * {@link cyanogenmod.app.SettingsManager} changing system settings.
+     *
+     * @see android.content.Context#getSystemService
+     * @see cyanogenmod.app.SettingsManager
+     *
+     * @hide
+     */
+    public static final String CM_SETTINGS_SERVICE = "cmsettings";
 }
