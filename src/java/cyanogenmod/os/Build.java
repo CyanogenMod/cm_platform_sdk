f68c6c7e98b46c3a016e25edb32e498a8930da5a
Adnan Begovic <adnan@cyngn.com>
2015-07-21 15:51:59 -0700

    cmsdk: Hide PARCELABLE_VERSION constant which varies between releases.
    Change-Id: I3bdeb9b359e46f0c02e833b23a958d071fc63df5

--- src/java/cyanogenmod/os/Build.java
+++ src/java/cyanogenmod/os/Build.java
@@ -34,6 +34,7 @@ public class Build {
      *
      * This parcelable version should be the latest version API version listed in
      * {@link CM_VERSION_CODES}
+     * @hide
      */
     public static final int PARCELABLE_VERSION = CM_VERSION_CODES.BOYSENBERRY;
 
