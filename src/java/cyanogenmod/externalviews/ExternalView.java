6701429faa81c15e2e6b9ebbc8c6f4b0f041c8fc
Danesh M <daneshm90@gmail.com>
2015-08-06 16:54:05 -0700

    Add external view
    Change-Id: Ic7f1ba96857239ac0fa43777f7d637716b7aead7

---
+++ src/java/cyanogenmod/externalviews/ExternalView.java
@@ -0,0 +1,237 @@
+package cyanogenmod.externalviews;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewTreeObserver;
+
+import java.util.LinkedList;
+
+public class ExternalView extends View implements ViewTreeObserver.OnScrollChangedListener,
+        Application.ActivityLifecycleCallbacks {
+
+    private Activity mActivity;
+    private final ComponentName mExtensionComponent;
+    private boolean mBound;
+    private LinkedList<Runnable> mQueue = new LinkedList<Runnable>();
+    private IExternalViewProvider mExternalViewProvider;
+
+    public ExternalView(Context context) {
+        this(context, (ComponentName) null);
+    }
+
+    public ExternalView(Context context, AttributeSet attrs) {
+        this(context, (ComponentName) null);
+    }
+
+    public ExternalView(Context context, AttributeSet attrs, int defStyleAttr) {
+        this(context, (ComponentName) null);
+    }
+
+    public ExternalView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        this(context, (ComponentName) null);
+    }
+
+    public ExternalView(Context context, ComponentName componentName) {
+        super(context);
+        mActivity = (Activity) getContext();
+        mExtensionComponent = componentName;
+        mActivity.getApplication().registerActivityLifecycleCallbacks(this);
+        bind();
+    }
+
+    private void bind() {
+        if (mBound) {
+            return;
+        }
+        Intent intent = new Intent();
+        intent.setComponent(mExtensionComponent);
+        mActivity.bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mBound = true;
+            mExternalViewProvider = IExternalViewProvider.Stub.asInterface(service);
+            executeQueue();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mBound = false;
+            mExternalViewProvider = null;
+        }
+    };
+
+    private void executeQueue() {
+        while (!mQueue.isEmpty()) {
+            Runnable r = mQueue.pop();
+            r.run();
+        }
+    }
+
+    // Activity lifecycle callbacks
+
+    @Override
+    public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+    }
+
+    private void performAction(Runnable r) {
+        if (mBound) {
+            r.run();
+        } else {
+            mQueue.add(r);
+        }
+    }
+    @Override
+    public void onActivityStarted(Activity activity) {
+        performAction(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    mExternalViewProvider.onStart();
+                } catch (RemoteException e) {
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResumed(Activity activity) {
+        performAction(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    mExternalViewProvider.onResume();
+                } catch (RemoteException e) {
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onActivityPaused(Activity activity) {
+        performAction(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    mExternalViewProvider.onPause();
+                } catch (RemoteException e) {
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onActivityStopped(Activity activity) {
+        performAction(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    mExternalViewProvider.onStop();
+                } catch (RemoteException e) {
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+    }
+
+    @Override
+    public void onActivityDestroyed(Activity activity) {
+        unbindExtension();
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        int[] screenCords = new int[2];
+        getLocationOnScreen(screenCords);
+        final Rect hitRect = new Rect();
+        mActivity.getWindow().getDecorView().getHitRect(hitRect);
+        final int x = screenCords[0];
+        final int y = screenCords[1];
+        final int width = getWidth();
+        final int height = getHeight();
+        performAction(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    mExternalViewProvider.alterWindow(x, y, width, height,
+                            getLocalVisibleRect(hitRect));
+                } catch (RemoteException e) {
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onScrollChanged() {
+//        int[] screenCords = new int[2];
+//        getLocationOnScreen(screenCords);
+//        final Rect hitRect = new Rect();
+//        mActivity.getWindow().getDecorView().getHitRect(hitRect);
+//        final int x = screenCords[0];
+//        final int y = screenCords[1];
+//        final int width = getWidth();
+//        final int height = getHeight();
+//        performAction(new Runnable() {
+//            @Override
+//            public void run() {
+//                try {
+//                    mExternalViewProvider.alterWindow(x, y, width, height,
+//                            getLocalVisibleRect(hitRect));
+//                } catch (RemoteException e) {
+//                }
+//            }
+//        });
+    }
+
+    // Placeholder callbacks
+
+    @Override
+    public void onDetachedFromWindow() {
+        performAction(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    mExternalViewProvider.onDetach();
+                } catch (RemoteException e) {
+                }
+            }
+        });
+        unbindExtension();
+    }
+
+    @Override
+    public void onAttachedToWindow() {
+        performAction(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    mExternalViewProvider.onAttach(null);
+                } catch (RemoteException e) {
+                }
+            }
+        });
+    }
+
+    private void unbindExtension() {
+        if (mBound) {
+            mBound = false;
+            mActivity.unbindService(mServiceConnection);
+        }
+    }
+}
