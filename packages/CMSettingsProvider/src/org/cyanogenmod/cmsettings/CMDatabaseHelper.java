cd6bcd0002c4e7dd20e5d2dabbeb0d3abf4bda09
Yvonne Wong <ywong@cyngn.com>
2015-08-27 12:19:54 -0700

    Add way to migrate CM specific settings to CMSettingsProvider
    issue-id: CYNGNOS-829
    
    Change-Id: I08743ebf9ffd3846ae501ed41e396b1556dc41cf

--- packages/CMSettingsProvider/src/org/cyanogenmod/cmsettings/CMDatabaseHelper.java
+++ packages/CMSettingsProvider/src/org/cyanogenmod/cmsettings/CMDatabaseHelper.java
@@ -16,12 +16,16 @@
 
 package org.cyanogenmod.cmsettings;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Build;
 import android.os.Environment;
 import android.os.UserHandle;
+import android.provider.Settings;
 import android.util.Log;
+import cyanogenmod.providers.CMSettings;
 
 import java.io.File;
 
@@ -50,6 +54,7 @@ public class CMDatabaseHelper extends SQLiteOpenHelper{
 
     private static final String CREATE_INDEX_SQL_FORMAT = "CREATE INDEX %sIndex%d ON %s (name);";
 
+    private Context mContext;
     private int mUserHandle;
 
     /**
@@ -77,11 +82,13 @@ public class CMDatabaseHelper extends SQLiteOpenHelper{
      */
     public CMDatabaseHelper(Context context, int userId) {
         super(context, dbNameForUser(userId), null, DATABASE_VERSION);
+        mContext = context;
         mUserHandle = userId;
     }
 
     /**
-     * Creates System, Secure, and Global tables in the specified {@link SQLiteDatabase}
+     * Creates System, Secure, and Global tables in the specified {@link SQLiteDatabase} and loads
+     * default values into the created tables.
      * @param db The database.
      */
     @Override
@@ -96,6 +103,8 @@ public class CMDatabaseHelper extends SQLiteOpenHelper{
                 createDbTable(db, CMTableNames.TABLE_GLOBAL);
             }
 
+            loadSettings(db);
+
             db.setTransactionSuccessful();
 
             if (LOCAL_LOGV) Log.v(TAG, "Successfully created tables for cm settings db");
@@ -106,8 +115,8 @@ public class CMDatabaseHelper extends SQLiteOpenHelper{
 
     /**
      * Creates a table and index for the specified database and table name
-     * @param db
-     * @param tableName
+     * @param db The {@link SQLiteDatabase} to create the table and index in.
+     * @param tableName The name of the database table to create.
      */
     private void createDbTable(SQLiteDatabase db, String tableName) {
         if (LOCAL_LOGV) Log.v(TAG, "Creating table and index for: " + tableName);
@@ -122,5 +131,106 @@ public class CMDatabaseHelper extends SQLiteOpenHelper{
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+    }
+
+    /**
+     * Loads default values for specific settings into the database.
+     * @param db The {@link SQLiteDatabase} to insert into.
+     */
+    private void loadSettings(SQLiteDatabase db) {
+        // System
+        loadIntegerSetting(db, CMTableNames.TABLE_SYSTEM, CMSettings.System.QS_QUICK_PULLDOWN,
+                R.integer.def_qs_quick_pulldown);
+
+        // Secure
+        loadBooleanSetting(db, CMTableNames.TABLE_SECURE, CMSettings.Secure.ADVANCED_MODE,
+                R.bool.def_advanced_mode);
+
+        loadStringSetting(db, CMTableNames.TABLE_SECURE, CMSettings.Secure.DEFAULT_THEME_COMPONENTS,
+                R.string.def_theme_components);
+
+        loadStringSetting(db, CMTableNames.TABLE_SECURE, CMSettings.Secure.DEFAULT_THEME_PACKAGE,
+                R.string.def_theme_package);
+
+        loadIntegerSetting(db, CMTableNames.TABLE_SECURE, CMSettings.Secure.DEV_FORCE_SHOW_NAVBAR,
+                R.integer.def_force_show_navbar);
+
+        loadStringSetting(db, CMTableNames.TABLE_SECURE, CMSettings.Secure.QS_TILES,
+                R.string.def_qs_tiles);
+
+        loadBooleanSetting(db, CMTableNames.TABLE_SECURE, CMSettings.Secure.STATS_COLLECTION,
+                R.bool.def_stats_collection);
+
+        // Global
+        if (mUserHandle == UserHandle.USER_OWNER) {
+            loadSettingsForTable(db, CMTableNames.TABLE_GLOBAL, CMSettings.Global.DEVICE_NAME,
+                    getDefaultDeviceName());
+
+            loadIntegerSetting(db, CMTableNames.TABLE_GLOBAL,
+                    CMSettings.Global.HEADS_UP_NOTIFICATIONS_ENABLED,
+                            R.integer.def_heads_up_enabled);
+        }
+    }
+
+    /**
+     * Loads a string resource into a database table. If a conflict occurs, that value is not
+     * inserted into the database table.
+     * @param db The {@link SQLiteDatabase} to insert into.
+     * @param tableName The name of the table to insert into.
+     * @param name The name of the value to insert into the table.
+     * @param resId The name of the string resource.
+     */
+    private void loadStringSetting(SQLiteDatabase db, String tableName, String name, int resId) {
+        loadSettingsForTable(db, tableName, name, mContext.getResources().getString(resId));
+    }
+
+    /**
+     * Loads a boolean resource into a database table. If a conflict occurs, that value is not
+     * inserted into the database table.
+     * @param db The {@link SQLiteDatabase} to insert into.
+     * @param tableName The name of the table to insert into.
+     * @param name The name of the value to insert into the table.
+     * @param resId The name of the boolean resource.
+     */
+    private void loadBooleanSetting(SQLiteDatabase db, String tableName, String name, int resId) {
+        loadSettingsForTable(db, tableName, name,
+                mContext.getResources().getBoolean(resId) ? "1" : "0");
+    }
+
+    /**
+     * Loads an integer resource into a database table. If a conflict occurs, that value is not
+     * inserted into the database table.
+     * @param db The {@link SQLiteDatabase} to insert into.
+     * @param tableName The name of the table to insert into.
+     * @param name The name of the value to insert into the table.
+     * @param resId The name of the integer resource.
+     */
+    private void loadIntegerSetting(SQLiteDatabase db, String tableName, String name, int resId) {
+        loadSettingsForTable(db, tableName, name,
+                Integer.toString(mContext.getResources().getInteger(resId)));
+    }
+
+    /**
+     * Loads a name/value pair into a database table. If a conflict occurs, that value is not
+     * inserted into the database table.
+     * @param db The {@link SQLiteDatabase} to insert into.
+     * @param tableName The name of the table to insert into.
+     * @param name The name of the value to insert into the table.
+     * @param value The value to insert into the table.
+     */
+    private void loadSettingsForTable(SQLiteDatabase db, String tableName, String name,
+            String value) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(Settings.NameValueTable.NAME, name);
+        contentValues.put(Settings.NameValueTable.VALUE, value);
+
+        db.insertWithOnConflict(tableName, null, contentValues, SQLiteDatabase.CONFLICT_IGNORE);
+    }
+
+    /**
+     * @return Gets the default device name
+     */
+    private String getDefaultDeviceName() {
+        return mContext.getResources().getString(R.string.def_device_name, Build.MODEL);
     }
 }
