9779dde99deb62aeee553a08cfb0e9f2b07239eb
Matt Garnes <matt@cyngn.com>
2015-09-05 14:25:07 -0700

    Add Broadcast action and query API for AudioSource.HOTWORD.
    - Add ACTION_HOTWORD_INPUT_CHANGED broadcast action and related extras.
    - Add getCurrentHotwordPackageName() API to SettingsManager to query the
      current package name of the application that controls the HOTWORD
      input.
    - Rename SettingsManager to PartnerInterface.
    
    Change-Id: I5987499cd32908c47a7e8e95d644c483dc32914c

--- tests/src/org/cyanogenmod/tests/settings/CMPartnerInterfaceTest.java
+++ tests/src/org/cyanogenmod/tests/settings/CMPartnerInterfaceTest.java
@@ -1,13 +1,13 @@
 package org.cyanogenmod.tests.settings;
 
 import org.cyanogenmod.tests.TestActivity;
-import cyanogenmod.app.SettingsManager;
+import cyanogenmod.app.PartnerInterface;
 
 /**
- * Tests functionality added in {@link cyanogenmod.app.SettingsManager}
+ * Tests functionality added in {@link cyanogenmod.app.PartnerInterface}
  */
-public class CMSettingsManagerTest extends TestActivity {
-    SettingsManager mSettingsManager;
+public class CMPartnerInterfaceTest extends TestActivity {
+    PartnerInterface mPartnerInterface;
     @Override
     protected String tag() {
         return null;
@@ -15,54 +15,54 @@ public class CMSettingsManagerTest extends TestActivity {
 
     @Override
     protected Test[] tests() {
-        mSettingsManager = SettingsManager.getInstance(this);
+        mPartnerInterface = PartnerInterface.getInstance(this);
         return mTests;
     }
 
     private Test[] mTests = new Test[] {
             new Test("Test set airplane mode to on") {
                 public void run() {
-                    mSettingsManager.setAirplaneModeEnabled(true);
+                    mPartnerInterface.setAirplaneModeEnabled(true);
                 }
             },
             new Test("Test set airplane mode to off") {
                 public void run() {
-                    mSettingsManager.setAirplaneModeEnabled(false);
+                    mPartnerInterface.setAirplaneModeEnabled(false);
                 }
             },
             new Test("Test set mobile data to on") {
                 public void run() {
-                    mSettingsManager.setMobileDataEnabled(true);
+                    mPartnerInterface.setMobileDataEnabled(true);
                 }
             },
             new Test("Test set mobile data to off") {
                 public void run() {
-                    mSettingsManager.setMobileDataEnabled(false);
+                    mPartnerInterface.setMobileDataEnabled(false);
                 }
             },
             new Test("Test reboot the device") {
                 public void run() {
-                    mSettingsManager.rebootDevice();
+                    mPartnerInterface.rebootDevice();
                 }
             },
             new Test("Test shutdown the device") {
                 public void run() {
-                    mSettingsManager.shutdownDevice();
+                    mPartnerInterface.shutdownDevice();
                 }
             },
             new Test("Test set zen mode to important interruptions") {
                 public void run() {
-                    mSettingsManager.setZenMode(SettingsManager.ZEN_MODE_IMPORTANT_INTERRUPTIONS);
+                    mPartnerInterface.setZenMode(PartnerInterface.ZEN_MODE_IMPORTANT_INTERRUPTIONS);
                 }
             },
             new Test("Test set zen mode to no interruptions") {
                 public void run() {
-                    mSettingsManager.setZenMode(SettingsManager.ZEN_MODE_NO_INTERRUPTIONS);
+                    mPartnerInterface.setZenMode(PartnerInterface.ZEN_MODE_NO_INTERRUPTIONS);
                 }
             },
             new Test("Test turn zen mode off") {
                 public void run() {
-                    mSettingsManager.setZenMode(SettingsManager.ZEN_MODE_OFF);
+                    mPartnerInterface.setZenMode(PartnerInterface.ZEN_MODE_OFF);
                 }
             },
     };
