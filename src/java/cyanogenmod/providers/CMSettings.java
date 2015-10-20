eef498255f26ca303422c68fdc6fcbdbbf841afa
AdrianDC <radian.dc@gmail.com>
2015-10-20 17:05:32 +0200

    HighTouchSensitivity [1/2]: Add Settings preference
     * Allows the HighTouchSensitivity (Glove mode)
       value to be saved in the CMSettings provider
    
     * Can be used in a device specific service to handle
       the Glove mode in a way closer to the device's vendor
    
     * Changes include :
         android_packages_apps_Settings
         cm_platform_sdk
    
    Conflicts:
    	src/java/cyanogenmod/providers/CMSettings.java
    
    RM-165
    Change-Id: Ib3cb8c4c67fb2397136ccfdf08cd8e7d938e898d
    Signed-off-by: AdrianDC <radian.dc@gmail.com>
    (cherry picked from commit 826c1aaf3ff30e138627d31e85193610df13c5ad)

--- src/java/cyanogenmod/providers/CMSettings.java
+++ src/java/cyanogenmod/providers/CMSettings.java
@@ -588,6 +588,14 @@ public final class CMSettings {
          */
         public static final String QS_QUICK_PULLDOWN = "qs_quick_pulldown";
 
+        /**
+         * Whether the HighTouchSensitivity is activated or not.
+         * 0 = off, 1 = on
+         * @hide
+         */
+        public static final String HIGH_TOUCH_SENSITIVITY_ENABLE =
+                "high_touch_sensitivity_enable";
+
         // endregion
     }
 
