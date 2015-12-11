6a125ffd6d943e7c424b4f926cfef86e062c9eba
Adnan Begovic <adnan@cyngn.com>
2015-12-10 17:03:07 -0800

    cmsdk: Nuke concept of Dynamic Tiles in available tiles.
    Change-Id: Iad95954603271dc42785cd4a1862200b2f8602f4

--- src/java/org/cyanogenmod/internal/util/QSUtils.java
+++ src/java/org/cyanogenmod/internal/util/QSUtils.java
@@ -62,10 +62,6 @@ public class QSUtils {
         return QSConstants.STATIC_TILES_AVAILABLE.contains(tileSpec);
     }
 
-    public static boolean isDynamicQsTile(String tileSpec) {
-        return QSConstants.DYNAMIC_TILES_AVAILABLE.contains(tileSpec);
-    }
-
     public static List<String> getAvailableTiles(Context context) {
         filterTiles(context);
         return QSConstants.TILES_AVAILABLE;
@@ -132,10 +128,6 @@ public class QSUtils {
                     break;
                 case QSConstants.TILE_BATTERY_SAVER:
                     removeTile = deviceSupportsPowerProfiles(context);
-                    break;
-
-                case QSConstants.DYNAMIC_TILE_SU:
-                    removeTile = !supportsRootAccess();
                     break;
             }
             if (removeTile) {
