3e897032dea92f4cd7362e533078069e3dfd930a
d34d <clark@cyngn.com>
2015-12-14 09:58:14 -0800

    ExtView: Allow window type and flags to be overridden
    KeyguardExternalViews need to have a different window type
    as well as different flags set when creating the window.  this
    patch allows those to be overridden when extending the
    ExternalViewProviderService.  This patch also makes sure that
    the width and height of the external view window is always the
    same as the display size.
    
    Change-Id: Ic5fc0d211e9fee0e610d55a3620e6e322defe231

--- src/java/cyanogenmod/externalviews/KeyguardExternalView.java
+++ src/java/cyanogenmod/externalviews/KeyguardExternalView.java
@@ -13,34 +13,72 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package cyanogenmod.externalviews;
 
 import android.content.Context;
 import android.content.ComponentName;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.os.RemoteException;
 import android.util.AttributeSet;
+import android.view.WindowManager;
 
+/**
+ * TODO: unhide once documented and finalized
+ * @hide
+ */
 public final class KeyguardExternalView extends ExternalView {
 
     public static final String EXTRA_PERMISSION_LIST = "permissions_list";
     public static final String CATEGORY_KEYGUARD_GRANT_PERMISSION
             = "org.cyanogenmod.intent.category.KEYGUARD_GRANT_PERMISSION";
 
+    private final Point mDisplaySize;
+
     public KeyguardExternalView(Context context, AttributeSet attrs) {
-        super(context,attrs);
+        this(context, attrs, null);
     }
 
     public KeyguardExternalView(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context,attrs,defStyleAttr);
+        this(context,attrs);
     }
 
     public KeyguardExternalView(Context context, AttributeSet attrs, int defStyleAttr,
             int defStyleRes) {
-        super(context,attrs,defStyleAttr,defStyleRes);
+        this(context,attrs);
     }
 
     public KeyguardExternalView(Context context, AttributeSet attributeSet,
             ComponentName componentName) {
         super(context,attributeSet,componentName);
+        mDisplaySize = new Point();
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        wm.getDefaultDisplay().getRealSize(mDisplaySize);
     }
 
+    @Override
+    public boolean onPreDraw() {
+        if (!mExternalViewProperties.hasChanged()) {
+            return true;
+        }
+        // keyguard views always take up the full screen when visible
+        final int x = mExternalViewProperties.getX();
+        final int y = mExternalViewProperties.getY();
+        final int width = mDisplaySize.x - x;
+        final int height = mDisplaySize.y - y;
+        final boolean visible = mExternalViewProperties.isVisible();
+        final Rect clipRect = new Rect(x, y, width + x, height + y);
+        performAction(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    mExternalViewProvider.alterWindow(x, y, width, height, visible,
+                            clipRect);
+                } catch (RemoteException e) {
+                }
+            }
+        });
+        return true;
+    }
 }
