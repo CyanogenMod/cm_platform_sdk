5bd64a59fd7ab4edef17312392a53ea346c748e8
Roman Birg <roman@cyngn.com>
2015-12-18 09:37:00 -0800

    Profiles: add isEnabled(); send state changes
    Ref: CYNGNOS-1461
    Change-Id: Idee9417a2f0b181c8d15cd763859c0a0e581dce6
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- cm/lib/main/java/org/cyanogenmod/platform/internal/ProfileManagerService.java
+++ cm/lib/main/java/org/cyanogenmod/platform/internal/ProfileManagerService.java
@@ -18,7 +18,11 @@ package org.cyanogenmod.platform.internal;
 
 import android.content.ComponentName;
 import android.content.ServiceConnection;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Message;
 import com.android.internal.policy.IKeyguardService;
+import cyanogenmod.providers.CMSettings;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
@@ -69,6 +73,8 @@ public class ProfileManagerService extends SystemService {
 
     /* package */ static final File PROFILE_FILE =
             new File(Environment.getSystemSecureDirectory(), "profiles.xml");
+
+    private static final int MSG_SEND_PROFILE_STATE = 10;
 
     private Map<UUID, Profile> mProfiles;
 
@@ -127,10 +133,41 @@ public class ProfileManagerService extends SystemService {
         }
     };
 
+    private final Handler.Callback mHandlerCallback = new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_SEND_PROFILE_STATE:
+                    Intent newState = new Intent(ProfileManager.PROFILES_STATE_CHANGED_ACTION);
+                    newState.putExtra(ProfileManager.EXTRA_PROFILES_STATE, msg.arg1);
+
+                    mContext.sendBroadcastAsUser(newState, UserHandle.ALL);
+
+
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    private class ProfilesObserver extends ContentObserver {
+        public ProfilesObserver(Handler handler) {
+            super(handler);
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            int state = CMSettings.System.getInt(mContext.getContentResolver(),
+                    CMSettings.System.SYSTEM_PROFILES_ENABLED,
+                    ProfileManager.PROFILES_STATE_ENABLED);
+            mHandler.obtainMessage(MSG_SEND_PROFILE_STATE, state, 0 /* unused */).sendToTarget();
+        }
+    }
+
     public ProfileManagerService(Context context) {
         super(context);
         mContext = context;
-        mHandler = new Handler();
+        mHandler = new Handler(mHandlerCallback);
         publishBinderService(CMContextConstants.CM_PROFILE_SERVICE, mService);
     }
 
@@ -149,6 +186,10 @@ public class ProfileManagerService extends SystemService {
         filter.addAction(Intent.ACTION_LOCALE_CHANGED);
         filter.addAction(Intent.ACTION_SHUTDOWN);
         mContext.registerReceiver(mIntentReceiver, filter);
+
+        mContext.getContentResolver().registerContentObserver(
+                CMSettings.System.getUriFor(CMSettings.System.SYSTEM_PROFILES_ENABLED),
+                false, new ProfilesObserver(mHandler), UserHandle.USER_ALL);
     }
 
     private void bindKeyguard() {
@@ -214,6 +255,10 @@ public class ProfileManagerService extends SystemService {
         @Override
         @Deprecated
         public boolean setActiveProfileByName(String profileName) {
+            if (!isEnabled()) {
+                Log.w(TAG, "Unable to set active profile because profiles are disabled.");
+                return false;
+            }
             if (!mProfileNames.containsKey(profileName)) {
                 // Since profileName could not be casted into a UUID, we can call it a string.
                 Log.w(TAG, "Unable to find profile to set active, based on string: " + profileName);
@@ -237,6 +282,10 @@ public class ProfileManagerService extends SystemService {
 
         @Override
         public boolean setActiveProfile(ParcelUuid profileParcelUuid) {
+            if (!isEnabled()) {
+                Log.w(TAG, "Unable to set active profile because profiles are disabled.");
+                return false;
+            }
             /*
              * We need to clear the caller's identity in order to
              * - allow the profile switch to execute actions
@@ -415,6 +464,14 @@ public class ProfileManagerService extends SystemService {
                 return mWildcardGroup;
             }
             return mGroups.get(uuid.getUuid());
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return CMSettings.System.getIntForUser(mContext.getContentResolver(),
+                    CMSettings.System.SYSTEM_PROFILES_ENABLED,
+                    ProfileManager.PROFILES_STATE_ENABLED,
+                    UserHandle.USER_CURRENT) == ProfileManager.PROFILES_STATE_ENABLED;
         }
     };
 
