a43b7560ada0dd16413d5dbdf983de3965167e07
d34d <clark@cyngn.com>
2015-12-21 10:34:15 -0800

    ExtView: Add keyguard specific interface for KeyguardExternalView
    KeyguardExternalView requires extra state callbacks that can be passed
    on to the KeyguardExternalViewProvider.  For this reason the
    KeyguardExternalViewProvider no longer extends ExternalViewProvider
    and instead provides the full implementation needed for ExternalViews
    as well as the specific keyguard components.
    
    Change-Id: Icf3589a201bf0ab29ba6e00e2bd6c0149e955e1d

--- src/java/cyanogenmod/externalviews/KeyguardExternalViewProviderService.java
+++ src/java/cyanogenmod/externalviews/KeyguardExternalViewProviderService.java
@@ -16,22 +16,264 @@
 
 package cyanogenmod.externalviews;
 
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 import android.view.WindowManager;
+import com.android.internal.policy.PhoneWindow;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
 
 /**
  * TODO: unhide once documented and finalized
  * @hide
  */
-public abstract class KeyguardExternalViewProviderService extends ExternalViewProviderService {
+public abstract class KeyguardExternalViewProviderService extends Service {
 
     private static final String TAG = KeyguardExternalViewProviderService.class.getSimpleName();
     private static final boolean DEBUG = false;
 
-    protected abstract class Provider extends ExternalViewProviderService.Provider {
-        protected Provider(Bundle options) {
-            super(options);
+    private WindowManager mWindowManager;
+    private final Handler mHandler = new Handler();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mWindowManager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+    }
+
+    @Override
+    public final IBinder onBind(Intent intent) {
+        return new IExternalViewProviderFactory.Stub() {
+            @Override public IBinder createExternalView(final Bundle options) {
+                FutureTask<IBinder> c = new FutureTask<IBinder>(new Callable<IBinder>() {
+                    @Override
+                    public IBinder call() throws Exception {
+                        return KeyguardExternalViewProviderService.this
+                                .createExternalView(options).mImpl;
+                    }
+                });
+                mHandler.post(c);
+                try {
+                    return c.get();
+                } catch (InterruptedException | ExecutionException e) {
+                    Log.e(TAG, "error: ", e);
+                    return null;
+                }
+            }
+        };
+    }
+
+    protected abstract Provider createExternalView(Bundle options);
+
+    protected abstract class Provider {
+        private final class ProviderImpl extends IKeyguardExternalViewProvider.Stub {
+            private final Window mWindow;
+            private final WindowManager.LayoutParams mParams;
+
+            private boolean mShouldShow = true;
+            private boolean mAskedShow = false;
+
+            public ProviderImpl(Provider provider) {
+                mWindow = new PhoneWindow(KeyguardExternalViewProviderService.this);
+                ((ViewGroup) mWindow.getDecorView()).addView(onCreateView());
+
+                mParams = new WindowManager.LayoutParams();
+                mParams.type = provider.getWindowType();
+                mParams.flags = provider.getWindowFlags();
+                mParams.gravity = Gravity.LEFT | Gravity.TOP;
+                mParams.format = PixelFormat.TRANSPARENT;
+            }
+
+            @Override
+            public void onAttach(IBinder windowToken) throws RemoteException {
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mWindowManager.addView(mWindow.getDecorView(), mParams);
+                        Provider.this.onAttach();
+                    }
+                });
+            }
+
+            @Override
+            public void onStart() throws RemoteException {
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Provider.this.onStart();
+                    }
+                });
+            }
+
+            @Override
+            public void onResume() throws RemoteException {
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mShouldShow = true;
+                        updateVisibility();
+                        Provider.this.onResume();
+                    }
+                });
+            }
+
+            @Override
+            public void onPause() throws RemoteException {
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mShouldShow = false;
+                        updateVisibility();
+                        Provider.this.onPause();
+                    }
+                });
+            }
+
+            @Override
+            public void onStop() throws RemoteException {
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Provider.this.onStop();
+                    }
+                });
+            }
+
+            @Override
+            public void onDetach() throws RemoteException {
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mWindowManager.removeView(mWindow.getDecorView());
+                        Provider.this.onDetach();
+                    }
+                });
+            }
+
+            @Override
+            public void onKeyguardShowing(final boolean screenOn) throws RemoteException {
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Provider.this.onKeyguardShowing(screenOn);
+                    }
+                });
+            }
+
+            @Override
+            public void onKeyguardDismissed() throws RemoteException {
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Provider.this.onKeyguardDismissed();
+                    }
+                });
+            }
+
+            @Override
+            public void onBouncerShowing(final boolean showing) throws RemoteException {
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Provider.this.onBouncerShowing(showing);
+                    }
+                });
+            }
+
+            @Override
+            public void onScreenTurnedOn() throws RemoteException {
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Provider.this.onScreenTurnedOn();
+                    }
+                });
+            }
+
+            @Override
+            public void onScreenTurnedOff() throws RemoteException {
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Provider.this.onScreenTurnedOff();
+                    }
+                });
+            }
+
+            @Override
+            public void alterWindow(final int x, final int y, final int width, final int height,
+                                    final boolean visible, final Rect clipRect) {
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mParams.x = x;
+                        mParams.y = y;
+                        mParams.width = width;
+                        mParams.height = height;
+
+                        if (DEBUG) Log.d(TAG, mParams.toString());
+
+                        mAskedShow = visible;
+
+                        updateVisibility();
+
+                        View decorView = mWindow.getDecorView();
+                        if (decorView.getVisibility() == View.VISIBLE) {
+                            decorView.setClipBounds(clipRect);
+                        }
+
+                        if (mWindow.getDecorView().getVisibility() != View.GONE)
+                            mWindowManager.updateViewLayout(mWindow.getDecorView(), mParams);
+                    }
+                });
+            }
+
+            private void updateVisibility() {
+                if (DEBUG) Log.d(TAG, "shouldShow = " + mShouldShow + " askedShow = " + mAskedShow);
+                mWindow.getDecorView().setVisibility(mShouldShow && mAskedShow ?
+                        View.VISIBLE : View.GONE);
+            }
         }
+
+        private final ProviderImpl mImpl = new ProviderImpl(this);
+        private final Bundle mOptions;
+
+        protected Provider(Bundle options) {
+            mOptions = options;
+        }
+
+        protected Bundle getOptions() {
+            return mOptions;
+        }
+
+        protected void onAttach() {}
+        protected abstract View onCreateView();
+        protected void onStart() {}
+        protected void onResume() {}
+        protected void onPause() {}
+        protected void onStop() {}
+        protected void onDetach() {}
+
+        protected abstract void onKeyguardShowing(boolean screenOn);
+        protected abstract void onKeyguardDismissed();
+        protected abstract void onBouncerShowing(boolean showing);
+        protected abstract void onScreenTurnedOn();
+        protected abstract void onScreenTurnedOff();
 
         /*package*/ final int getWindowType() {
             return WindowManager.LayoutParams.TYPE_KEYGUARD_PANEL;
