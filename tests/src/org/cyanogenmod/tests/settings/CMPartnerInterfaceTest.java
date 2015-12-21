83942353dba00642707a26cb5231afc36de6da60
Janet Sun <jsun@cyngn.com>
2015-12-21 14:35:31 -0800

    CMSDK: Add support to turn on priority/quiet mode for a certain duration Call NotificationManager.setZenMode with a valid Condition URI containing end time. Duration values <= 0 will result in 0 duration. Values causing MAX_LONG overflow will be treated as indefinite duration.
    FOR-156
    
    Change-Id: I18dd661fc07419a7019b6372e81df430a70f86e9

--- tests/src/org/cyanogenmod/tests/settings/CMPartnerInterfaceTest.java
+++ tests/src/org/cyanogenmod/tests/settings/CMPartnerInterfaceTest.java
@@ -7,6 +7,12 @@ import cyanogenmod.app.PartnerInterface;
  * Tests functionality added in {@link cyanogenmod.app.PartnerInterface}
  */
 public class CMPartnerInterfaceTest extends TestActivity {
+
+    // Zen Mode to 15 minutes
+    private static final long ZEN_MODE_DURATION_15_MINUTES_MS = 15 * 60000;
+    // Zen Mode to 1 hour
+    private static final long ZEN_MODE_DURATION_1_HOUR_MS = 4 * ZEN_MODE_DURATION_15_MINUTES_MS;
+
     PartnerInterface mPartnerInterface;
     @Override
     protected String tag() {
@@ -63,6 +69,47 @@ public class CMPartnerInterfaceTest extends TestActivity {
             new Test("Test turn zen mode off") {
                 public void run() {
                     mPartnerInterface.setZenMode(PartnerInterface.ZEN_MODE_OFF);
+                }
+            },
+
+            new Test("Test set zen mode to important interruptions with 15 mins duration") {
+                public void run() {
+                    mPartnerInterface.setZenModeWithDuration(PartnerInterface.ZEN_MODE_IMPORTANT_INTERRUPTIONS, ZEN_MODE_DURATION_15_MINUTES_MS);
+                }
+            },
+            new Test("Test set zen mode to no interruptions with 15 mins duration") {
+                public void run() {
+                    mPartnerInterface.setZenModeWithDuration(PartnerInterface.ZEN_MODE_NO_INTERRUPTIONS, ZEN_MODE_DURATION_15_MINUTES_MS);
+                }
+            },
+            new Test("Test set zen mode to important interruptions with 1 hour 15 mins duration") {
+                public void run() {
+                    mPartnerInterface.setZenModeWithDuration(PartnerInterface.ZEN_MODE_IMPORTANT_INTERRUPTIONS, ZEN_MODE_DURATION_1_HOUR_MS + ZEN_MODE_DURATION_15_MINUTES_MS);
+                }
+            },
+            new Test("Test set zen mode to no interruptions with 1 hour 15 mins duration") {
+                public void run() {
+                    mPartnerInterface.setZenModeWithDuration(PartnerInterface.ZEN_MODE_NO_INTERRUPTIONS, ZEN_MODE_DURATION_1_HOUR_MS + ZEN_MODE_DURATION_15_MINUTES_MS);
+                }
+            },
+            new Test("Test set zen mode to important interruptions with negative duration") {
+                public void run() {
+                    mPartnerInterface.setZenModeWithDuration(PartnerInterface.ZEN_MODE_IMPORTANT_INTERRUPTIONS, -1);
+                }
+            },
+            new Test("Test set zen mode to no interruptions with negative duration") {
+                public void run() {
+                    mPartnerInterface.setZenModeWithDuration(PartnerInterface.ZEN_MODE_NO_INTERRUPTIONS, -1);
+                }
+            },
+            new Test("Test set zen mode to important interruptions with MAX_LONG duration") {
+                public void run() {
+                    mPartnerInterface.setZenModeWithDuration(PartnerInterface.ZEN_MODE_IMPORTANT_INTERRUPTIONS, Long.MAX_VALUE);
+                }
+            },
+            new Test("Test set zen mode to no interruptions with MAX_LONG duration") {
+                public void run() {
+                    mPartnerInterface.setZenModeWithDuration(PartnerInterface.ZEN_MODE_NO_INTERRUPTIONS, Long.MAX_VALUE);
                 }
             },
     };
