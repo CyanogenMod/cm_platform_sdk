bd5c448a7503ea1b2f1592264a86f4b1c6ed8175
Adnan Begovic <adnan@cyngn.com>
2016-01-05 15:47:15 -0800

    cmsdk: Add example service test.
    Change-Id: I52972bc4d32505ba7fd2f7278efdb9441c39b82a

--- tests/Android.mk
+++ tests/Android.mk
@@ -19,7 +19,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := tests
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
-    org.cyanogenmod.platform.sdk
+    org.cyanogenmod.platform
 
 LOCAL_SRC_FILES := $(call all-subdir-java-files, src/)
 
