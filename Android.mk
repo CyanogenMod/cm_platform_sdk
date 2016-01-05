bbab82e97b59418b2bf089be61727581ace0ed02
Adnan Begovic <adnan@cyngn.com>
2016-01-05 14:15:09 -0800

    cmsdk: Add cmsdk non-jack target module.
      Since LOCAL_JACK_ENABLED isn't flexible enough
      to have generation of both java archives and jack
      binaries, we can work around this by having a
      secondary module defined that disables jack compile.
    
    Change-Id: Ied647b659c13b672621d82af536d1ed447d5f7fa

--- Android.mk
+++ Android.mk
@@ -123,6 +123,33 @@ LOCAL_INTERMEDIATE_SOURCES := \
 $(full_target): $(cm_framework_built) $(gen)
 include $(BUILD_STATIC_JAVA_LIBRARY)
 
+# the sdk as a jar for publish, not built as part of full target
+# DO NOT LINK AGAINST THIS IN BUILD
+# ============================================================
+include $(CLEAR_VARS)
+
+LOCAL_MODULE:= org.cyanogenmod.platform.sdk.jar
+LOCAL_MODULE_TAGS := optional
+LOCAL_REQUIRED_MODULES := services
+LOCAL_JACK_ENABLED := disabled
+
+LOCAL_SRC_FILES := \
+    $(call all-java-files-under, $(cyanogenmod_src)) \
+    $(call all-Iaidl-files-under, $(cyanogenmod_src)) \
+    $(call all-Iaidl-files-under, $(cyanogenmod_internal_src))
+
+# Included aidl files from cyanogenmod.app namespace
+LOCAL_AIDL_INCLUDES := $(LOCAL_PATH)/src/java
+
+cmsdk_LOCAL_INTERMEDIATE_SOURCES := \
+    $(cm_platform_res)/cyanogenmod/platform/R.java \
+    $(cm_platform_res)/cyanogenmod/platform/Manifest.java
+
+LOCAL_INTERMEDIATE_SOURCES := \
+    $(cmsdk_LOCAL_INTERMEDIATE_SOURCES)
+
+include $(BUILD_STATIC_JAVA_LIBRARY)
+
 # full target for use by platform apps
 #
 include $(CLEAR_VARS)
