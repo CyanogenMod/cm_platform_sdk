348cbdf671b63197cf543f7bf814428f8b65675c
Adnan Begovic <adnan@cyngn.com>
2016-01-15 15:48:39 -0800

    cmsdk: Add CYANOGENMOD_VERSION constant.
    TICKET: CYNGNOS-1625
    Change-Id: I81526ae7d90e453975e2c1e2fc79234f706fb415
    (cherry picked from commit cf3f38c37c9bec6140bc6a3b1bcddf65e430c1f5)

--- src/java/cyanogenmod/os/Build.java
+++ src/java/cyanogenmod/os/Build.java
@@ -38,6 +38,9 @@ public class Build {
      */
     public static final int PARCELABLE_VERSION = CM_VERSION_CODES.DRAGON_FRUIT;
 
+    /** A build ID utilized to distinguish cyanogenmod versions */
+    public static final String CYANOGENMOD_VERSION = "ro.cm.version";
+
     /** A build ID string meant for displaying to the user */
     public static final String CYANOGENMOD_DISPLAY_VERSION = "ro.cm.display.version";
 
