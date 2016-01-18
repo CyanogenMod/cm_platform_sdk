24f2bb4235125f19f778cdf5ba06ee591af08553
Adnan Begovic <adnan@cyngn.com>
2016-01-18 11:26:42 -0800

    cmsdk: Introduce Elderberry, api level 5.
    Change-Id: I916cb96e86e7dcee04b3b4cd08fec934b1ce5876

--- src/java/cyanogenmod/os/Build.java
+++ src/java/cyanogenmod/os/Build.java
@@ -136,6 +136,11 @@ public class Build {
          * </ul>
          */
         public static final int DRAGON_FRUIT = 4;
+
+        /**
+         * Future M Release - 2nd quarter 2016
+         */
+        public static final int ELDERBERRY = 5;
     }
 
     /**
