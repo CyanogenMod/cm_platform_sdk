cff7fae1730bc643b11ec744b24c06a8cacf99e0
d34d <clark@cyngn.com>
2016-01-21 16:57:42 -0800

    ExtViews: Add action and meta-data
    Services that extend KeyguardExternalViewProviderService should also
    include this newly defined action in an intent-filter within the
    <service> tag in their AndroidManifest.xml.  These services can also
    include meta-data that defines a configuration xml which will be used
    to define a settings activity that can be launched within a yet to
    be defined settings/configuration UI.
    
    Change-Id: I0a5ca09cf6f63413d067f89a9757137102dd09f6
    TICKET: CYNGNOS-1687

--- src/java/cyanogenmod/externalviews/KeyguardExternalViewProviderService.java
+++ src/java/cyanogenmod/externalviews/KeyguardExternalViewProviderService.java
@@ -57,6 +57,18 @@ public abstract class KeyguardExternalViewProviderService extends Service {
     private static final String TAG = KeyguardExternalViewProviderService.class.getSimpleName();
     private static final boolean DEBUG = false;
 
+    /**
+     * The action that must be declared as handled by this service.
+     *
+     * <p>{@code
+     *  <intent-filter>
+     *      <action android:name="cyanogenmod.externalviews.KeyguardExternalViewProviderService"/>
+     *  </intent-filter>
+     *}</p>
+     */
+    public static final String SERVICE_INTERFACE =
+            "cyanogenmod.externalviews.KeyguardExternalViewProviderService";
+
     private WindowManager mWindowManager;
     private final Handler mHandler = new Handler();
 
