10e966bbe7fc6b47bb28b7dbf5c46ae76c7933cf
Danesh M <daneshm90@gmail.com>
2016-02-08 14:57:31 -0800

    CMSettings : Add option to pass swipe-up-to-unlock (3/3)
    CYNGNOS-1873
    
    Change-Id: Icdd2505dbc56a77f49a76ea61daf75c5d8a24fd6
    (cherry picked from commit 2d1d7239a52561f7a11197b29ad0fcd6e5d4ed33)

--- src/java/cyanogenmod/providers/CMSettings.java
+++ src/java/cyanogenmod/providers/CMSettings.java
@@ -2591,6 +2591,12 @@ public final class CMSettings {
          */
         public static final String LIVE_LOCK_SCREEN_ENABLED = "live_lock_screen_enabled";
 
+        /**
+         * Whether keyguard will direct show security view (0 = false, 1 = true)
+         * @hide
+         */
+        public static final String LOCK_PASS_TO_SECURITY_VIEW = "lock_screen_pass_to_security_view";
+
         // endregion
 
         /**
@@ -2637,6 +2643,7 @@ public final class CMSettings {
                 CMSettings.Secure.APP_PERFORMANCE_PROFILES_ENABLED,
                 CMSettings.Secure.QS_LOCATION_ADVANCED,
                 CMSettings.Secure.LOCKSCREEN_VISUALIZER_ENABLED,
+                CMSettings.Secure.LOCK_PASS_TO_SECURITY_VIEW
         };
 
         /**
