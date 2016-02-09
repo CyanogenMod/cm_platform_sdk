fcadbf539cd1b78bd9c731212c7668e431ebf441
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-02-10 01:39:17 +0200

    cmsdk: launch default music player on headset connect [3/3]
    Change-Id: Ibbb0607a56ab4d9246c14632eb4199558866854a

--- src/java/cyanogenmod/providers/CMSettings.java
+++ src/java/cyanogenmod/providers/CMSettings.java
@@ -1727,6 +1727,14 @@ public final class CMSettings {
                 sBooleanValidator;
 
         /**
+         * Whether or not to launch default music player when headset is connected
+         */
+        public static final String HEADSET_CONNECT_PLAYER = "headset_connect_player";
+
+        /** @hide */
+        public static final Validator HEADSET_CONNECT_PLAYER_VALIDATOR = sBooleanValidator;
+
+        /**
          * I can haz more bukkits
          * @hide
          */
@@ -1829,6 +1837,7 @@ public final class CMSettings {
                 CMSettings.System.STATUS_BAR_SHOW_BATTERY_PERCENT,
                 CMSettings.System.VOLUME_KEYS_CONTROL_RING_STREAM,
                 CMSettings.System.NAVIGATION_BAR_MENU_ARROW_KEYS,
+                CMSettings.System.HEADSET_CONNECT_PLAYER,
         };
 
         /**
@@ -1973,6 +1982,7 @@ public final class CMSettings {
                     NOTIFICATION_LIGHT_PULSE_CUSTOM_VALUES_VALIDATOR);
             VALIDATORS.put(NOTIFICATION_LIGHT_COLOR_AUTO,
                     NOTIFICATION_LIGHT_COLOR_AUTO_VALIDATOR);
+            VALIDATORS.put(HEADSET_CONNECT_PLAYER, HEADSET_CONNECT_PLAYER_VALIDATOR);
             VALIDATORS.put(__MAGICAL_TEST_PASSING_ENABLER,
                     __MAGICAL_TEST_PASSING_ENABLER_VALIDATOR);
         };
