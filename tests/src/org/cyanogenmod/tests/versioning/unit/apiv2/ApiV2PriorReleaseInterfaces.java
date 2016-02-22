a305b8777db302309b2b907de125574f925dd61f
Adnan Begovic <adnan@cyngn.com>
2016-02-22 14:12:05 -0800

    cmsdk: Update binder transaction tests for new interfaces.
    Change-Id: I79352dc36a4934f9821c5b487df0fb19a7ebc693

--- tests/src/org/cyanogenmod/tests/versioning/unit/apiv2/ApiV2PriorReleaseInterfaces.java
+++ tests/src/org/cyanogenmod/tests/versioning/unit/apiv2/ApiV2PriorReleaseInterfaces.java
@@ -15,6 +15,7 @@
  */
 
 package org.cyanogenmod.tests.versioning.unit.apiv2;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,97 +23,121 @@ import java.util.Map;
  * Created by adnan on 2/4/16.
  */
 public class ApiV2PriorReleaseInterfaces {
-    private static Map<String, Integer> mApiMethodsAndValues = new HashMap<String, Integer>();
+    private static Map<String, Map<String, Integer>> mApiMethodsAndValues =
+            new HashMap<String, Map<String, Integer>>();
 
     //Profiles Aidl (IProfileManager)
     static {
+        Map<String, Integer> profilesMap = getInternalInterfaceMap("IProfileManager");
         // APRICOT + BOYSENBERRY + CANTALOUPE
-        mApiMethodsAndValues.put("setActiveProfile", 1);
-        mApiMethodsAndValues.put("etActiveProfileByName", 2);
-        mApiMethodsAndValues.put("getActiveProfile", 3);
-        mApiMethodsAndValues.put("addProfile", 4);
-        mApiMethodsAndValues.put("removeProfile", 5);
-        mApiMethodsAndValues.put("updateProfile", 6);
-        mApiMethodsAndValues.put("getProfile", 7);
-        mApiMethodsAndValues.put("getProfileByName", 8);
-        mApiMethodsAndValues.put("getProfiles", 9);
-        mApiMethodsAndValues.put("profileExists", 10);
-        mApiMethodsAndValues.put("profileExistsByName", 11);
-        mApiMethodsAndValues.put("notificationGroupExistsByName", 12);
-        mApiMethodsAndValues.put("getNotificationGroups", 13);
-        mApiMethodsAndValues.put("addNotificationGroup", 14);
-        mApiMethodsAndValues.put("removeNotificationGroup", 15);
-        mApiMethodsAndValues.put("updateNotificationGroup", 16);
-        mApiMethodsAndValues.put("getNotificationGroupForPackage", 17);
-        mApiMethodsAndValues.put("getNotificationGroup", 18);
-        mApiMethodsAndValues.put("resetAll", 19);
-
-        //FUTURE RELEASE
+        profilesMap.put("setActiveProfile", 1);
+        profilesMap.put("etActiveProfileByName", 2);
+        profilesMap.put("getActiveProfile", 3);
+        profilesMap.put("addProfile", 4);
+        profilesMap.put("removeProfile", 5);
+        profilesMap.put("updateProfile", 6);
+        profilesMap.put("getProfile", 7);
+        profilesMap.put("getProfileByName", 8);
+        profilesMap.put("getProfiles", 9);
+        profilesMap.put("profileExists", 10);
+        profilesMap.put("profileExistsByName", 11);
+        profilesMap.put("notificationGroupExistsByName", 12);
+        profilesMap.put("getNotificationGroups", 13);
+        profilesMap.put("addNotificationGroup", 14);
+        profilesMap.put("removeNotificationGroup", 15);
+        profilesMap.put("updateNotificationGroup", 16);
+        profilesMap.put("getNotificationGroupForPackage", 17);
+        profilesMap.put("getNotificationGroup", 18);
+        profilesMap.put("resetAll", 19);
     }
 
     //PartnerInterface Aidl (IPartnerInterface)
     static {
+        Map<String, Integer> partnerMap = getInternalInterfaceMap("IPartnerInterface");
         // APRICOT + BOYSENBERRY + CANTALOUPE
-        mApiMethodsAndValues.put("setAirplaneModeEnabled_0", 1);
-        mApiMethodsAndValues.put("setMobileDataEnabled_1", 2);
-        mApiMethodsAndValues.put("setZenMode", 3);
-        mApiMethodsAndValues.put("shutdown", 4);
-        mApiMethodsAndValues.put("reboot", 5);
-        mApiMethodsAndValues.put("getCurrentHotwordPackageName", 6);
-
-        //FUTURE RELEASE
+        partnerMap.put("setAirplaneModeEnabled", 1);
+        partnerMap.put("setMobileDataEnabled", 2);
+        partnerMap.put("setZenMode", 3);
+        partnerMap.put("shutdown", 4);
+        partnerMap.put("reboot", 5);
+        partnerMap.put("getCurrentHotwordPackageName", 6);
     }
 
     //CMHardwareManager Aidl (ICMHardwareService)
     static {
+        Map<String, Integer> hardwareMap = getInternalInterfaceMap("ICMHardwareService");
         // APRICOT + BOYSENBERRY + CANTALOUPE
-        mApiMethodsAndValues.put("getSupportedFeatures_0", 1);
-        mApiMethodsAndValues.put("get_1", 2);
-        mApiMethodsAndValues.put("set", 3);
-        mApiMethodsAndValues.put("getDisplayColorCalibration", 4);
-        mApiMethodsAndValues.put("setDisplayColorCalibration", 5);
-        mApiMethodsAndValues.put("getNumGammaControls", 6);
-        mApiMethodsAndValues.put("getDisplayGammaCalibration", 7);
-        mApiMethodsAndValues.put("setDisplayGammaCalibration", 8);
-        mApiMethodsAndValues.put("getVibratorIntensity", 9);
-        mApiMethodsAndValues.put("setVibratorIntensity", 10);
-        mApiMethodsAndValues.put("getLtoSource", 11);
-        mApiMethodsAndValues.put("getLtoDestination", 12);
-        mApiMethodsAndValues.put("getLtoDownloadInterval", 13);
-        mApiMethodsAndValues.put("getSerialNumber", 14);
-        mApiMethodsAndValues.put("requireAdaptiveBacklightForSunlightEnhancement", 15);
-        mApiMethodsAndValues.put("getDisplayModes", 16);
-        mApiMethodsAndValues.put("getCurrentDisplayMode", 17);
-        mApiMethodsAndValues.put("getDefaultDisplayMode", 18);
-        mApiMethodsAndValues.put("setDisplayMode", 19);
-        mApiMethodsAndValues.put("writePersistentBytes", 20);
-        mApiMethodsAndValues.put("readPersistentBytes", 21);
-        mApiMethodsAndValues.put("getThermalState", 22);
-        mApiMethodsAndValues.put("registerThermalListener", 23);
-        mApiMethodsAndValues.put("unRegisterThermalListener", 24);
-
-        //FUTURE RELEASE
+        hardwareMap.put("getSupportedFeatures", 1);
+        hardwareMap.put("get", 2);
+        hardwareMap.put("set", 3);
+        hardwareMap.put("getDisplayColorCalibration", 4);
+        hardwareMap.put("setDisplayColorCalibration", 5);
+        hardwareMap.put("getNumGammaControls", 6);
+        hardwareMap.put("getDisplayGammaCalibration", 7);
+        hardwareMap.put("setDisplayGammaCalibration", 8);
+        hardwareMap.put("getVibratorIntensity", 9);
+        hardwareMap.put("setVibratorIntensity", 10);
+        hardwareMap.put("getLtoSource", 11);
+        hardwareMap.put("getLtoDestination", 12);
+        hardwareMap.put("getLtoDownloadInterval", 13);
+        hardwareMap.put("getSerialNumber", 14);
+        hardwareMap.put("requireAdaptiveBacklightForSunlightEnhancement", 15);
+        hardwareMap.put("getDisplayModes", 16);
+        hardwareMap.put("getCurrentDisplayMode", 17);
+        hardwareMap.put("getDefaultDisplayMode", 18);
+        hardwareMap.put("setDisplayMode", 19);
+        hardwareMap.put("writePersistentBytes", 20);
+        hardwareMap.put("readPersistentBytes", 21);
+        hardwareMap.put("getThermalState", 22);
+        hardwareMap.put("registerThermalListener", 23);
+        hardwareMap.put("unRegisterThermalListener", 24);
     }
 
     //CMStatusBarManager Aidl (ICMStatusBarManager)
     static {
+        Map<String, Integer> statusBarMap = getInternalInterfaceMap("ICMStatusBarManager");
         // APRICOT + BOYSENBERRY + CANTALOUPE
-        mApiMethodsAndValues.put("createCustomTileWithTag", 1);
-        mApiMethodsAndValues.put("removeCustomTileWithTag", 2);
-        mApiMethodsAndValues.put("registerListener", 3);
-        mApiMethodsAndValues.put("unregisterListener", 4);
-        mApiMethodsAndValues.put("removeCustomTileFromListener", 5);
-
-        //FUTURE RELEASE
+        statusBarMap.put("createCustomTileWithTag", 1);
+        statusBarMap.put("removeCustomTileWithTag", 2);
+        statusBarMap.put("registerListener", 3);
+        statusBarMap.put("unregisterListener", 4);
+        statusBarMap.put("removeCustomTileFromListener", 5);
     }
 
     //AppSuggestManager Aidl (IAppSuggestManager)
     static {
-        mApiMethodsAndValues.put("handles_0", 1);
-        mApiMethodsAndValues.put("getSuggestions_1", 2);
+        Map<String, Integer> suggestMap = getInternalInterfaceMap("IAppSuggestManager");
+        // APRICOT + BOYSENBERRY + CANTALOUPE
+        suggestMap.put("handles", 1);
+        suggestMap.put("getSuggestions", 2);
     }
 
-    public static Map<String, Integer> getInterfaces() {
+    //CMTelephonyManager Aidl (ICMTelephonyManager)
+    static {
+        Map<String, Integer> telephonyMap = getInternalInterfaceMap("ICMTelephonyManager");
+        // APRICOT + BOYSENBERRY + CANTALOUPE
+        telephonyMap.put("getSubInformation", 1);
+        telephonyMap.put("isSubActive", 2);
+        telephonyMap.put("isDataConnectionSelectedOnSub", 3);
+        telephonyMap.put("isDataConnectionEnabled", 4);
+        telephonyMap.put("setSubState", 5);
+        telephonyMap.put("setDataConnectionSelectedOnSub", 6);
+        telephonyMap.put("setDataConnectionState", 7);
+        telephonyMap.put("setDefaultPhoneSub", 8);
+        telephonyMap.put("setDefaultSmsSub", 9);
+    }
+
+    protected static Map<String, Integer> getInternalInterfaceMap(String targetInterface) {
+        Map<String, Integer> internalMap = mApiMethodsAndValues.get(targetInterface);
+        if (internalMap == null) {
+            internalMap = new HashMap<String, Integer>();
+            mApiMethodsAndValues.put(targetInterface, internalMap);
+            return internalMap;
+        }
+        return internalMap;
+    }
+
+    public static Map<String, Map<String, Integer>> getInterfaces() {
         return mApiMethodsAndValues;
     }
 }
