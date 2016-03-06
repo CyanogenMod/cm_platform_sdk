d63d65f423d899fda3c832b6ae17cf48d3e353cf
Dan Elkouby <streetwalrus@codewalr.us>
2016-03-06 11:37:39 +0200

    Notification slider: add option to ignore slider in zen auto mode
    Change-Id: I4bfade992ec1136f32656423c6776ea2a8568e32

--- src/java/cyanogenmod/providers/CMSettings.java
+++ src/java/cyanogenmod/providers/CMSettings.java
@@ -1761,6 +1761,16 @@ public final class CMSettings {
                 sBooleanValidator;
 
         /**
+         * Whether or not to ignore the notification slider when zen is in auto mode
+         */
+        public static final String NOTIFICATION_SLIDER_IGNORE_AUTO =
+                "notification_slider_ignore_auto";
+
+        /** @hide */
+        public static final Validator NOTIFICATION_SLIDER_IGNORE_AUTO_VALIDATOR =
+                sBooleanValidator;
+
+        /**
          * I can haz more bukkits
          * @hide
          */
@@ -1866,6 +1876,7 @@ public final class CMSettings {
                 CMSettings.System.HEADSET_CONNECT_PLAYER,
                 CMSettings.System.ZEN_ALLOW_LIGHTS,
                 CMSettings.System.TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK,
+                CMSettings.System.NOTIFICATION_SLIDER_IGNORE_AUTO,
         };
 
         /**
@@ -2015,6 +2026,8 @@ public final class CMSettings {
             VALIDATORS.put(ZEN_PRIORITY_ALLOW_LIGHTS, ZEN_PRIORITY_ALLOW_LIGHTS_VALIDATOR);
             VALIDATORS.put(TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK,
                     TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK_VALIDATOR);
+            VALIDATORS.put(NOTIFICATION_SLIDER_IGNORE_AUTO,
+                    NOTIFICATION_SLIDER_IGNORE_AUTO_VALIDATOR);
             VALIDATORS.put(__MAGICAL_TEST_PASSING_ENABLER,
                     __MAGICAL_TEST_PASSING_ENABLER_VALIDATOR);
         };
