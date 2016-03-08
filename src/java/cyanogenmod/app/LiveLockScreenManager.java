81708e1bf42331b0414ec783ef5a4758e7475380
d34d <clark@cyngn.com>
2016-03-08 09:14:34 -0800

    LLS: Add live lock screen service
    The live lock screen service is resposonsible for deciding what
    LLS should be displayed at any given time.  Live lock screens can
    be swapped out using a priority based system.
    
    Change-Id: Ifba73e839b749fe78a9e4ee347dd20eea6bf0a22

---
+++ src/java/cyanogenmod/app/LiveLockScreenManager.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cyanogenmod.app;
+
+import android.annotation.NonNull;
+import android.annotation.Nullable;
+import android.annotation.SdkConstant;
+import android.content.Context;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.util.Log;
+
+/** {@hide} */
+public class LiveLockScreenManager {
+    private static final String TAG = LiveLockScreenManager.class.getSimpleName();
+    private static ILiveLockScreenManager sService;
+    private static LiveLockScreenManager sInstance;
+
+    private Context mContext;
+
+    /**
+     * The {@link android.content.Intent} that must be declared as handled by the service.
+     */
+    @SdkConstant(SdkConstant.SdkConstantType.SERVICE_ACTION)
+    public static final String SERVICE_INTERFACE
+            = "cyanogenmod.app.LiveLockScreenManagerService";
+
+    private LiveLockScreenManager(Context context) {
+        mContext = context;
+        sService = getService();
+    }
+
+    private ILiveLockScreenManager getService() {
+        if (sService == null) {
+            IBinder b = ServiceManager.getService(CMContextConstants.CM_LIVE_LOCK_SCREEN_SERVICE);
+            if (b != null) {
+                sService = ILiveLockScreenManager.Stub.asInterface(b);
+            }
+        }
+
+        return sService;
+    }
+
+    private void logServiceException(Exception e) {
+        Log.w(TAG, "Unable to access LiveLockScreenServiceBroker", e);
+    }
+
+    public static LiveLockScreenManager getInstance(Context context) {
+        if (sInstance == null) {
+            sInstance = new LiveLockScreenManager(context);
+        }
+
+        return sInstance;
+    }
+
+    public void show(int id, @NonNull LiveLockScreenInfo lls) {
+        String pkg = mContext.getPackageName();
+        try {
+            sService.enqueueLiveLockScreen(pkg, id, lls);
+        } catch (RemoteException e) {
+            logServiceException(e);
+        }
+    }
+
+    public void cancel(int id) {
+        String pkg = mContext.getPackageName();
+        try {
+            sService.cancelLiveLockScreen(pkg, id);
+        } catch (RemoteException e) {
+            logServiceException(e);
+        }
+    }
+
+    /** @hide */
+    public LiveLockScreenInfo getCurrentLiveLockScreen() {
+        LiveLockScreenInfo lls = null;
+        try {
+            lls = sService.getCurrentLiveLockScreen();
+        } catch (RemoteException e) {
+            logServiceException(e);
+        }
+
+        return lls;
+    }
+
+    /** @hide */
+    public boolean getLiveLockScreenEnabled() {
+        try {
+            return sService.getLiveLockScreenEnabled();
+        } catch (RemoteException e) {
+            logServiceException(e);
+        }
+
+        return false;
+    }
+
+    /** @hide */
+    public void setLiveLockScreenEnabled(boolean enabled) {
+        try {
+            sService.setLiveLockScreenEnabled(enabled);
+        } catch (RemoteException e) {
+            logServiceException(e);
+        }
+    }
+
+    /** @hide */
+    public void setDefaultLiveLockScreen(@Nullable LiveLockScreenInfo llsInfo) {
+        try {
+            sService.setDefaultLiveLockScreen(llsInfo);
+        } catch (RemoteException e) {
+            logServiceException(e);
+        }
+    }
+
+    /** @hide */
+    public LiveLockScreenInfo getDefaultLiveLockScreen() {
+        try {
+            return sService.getDefaultLiveLockScreen();
+        } catch (RemoteException e) {
+            logServiceException(e);
+        }
+
+        return null;
+    }
+}
