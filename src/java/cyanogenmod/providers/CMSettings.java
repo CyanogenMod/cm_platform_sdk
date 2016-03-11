5e2d5a685036ac8685bc9375addccd2bc40838a3
d34d <clark@cyngn.com>
2016-03-11 13:16:12 -0800

    CMSettings: Add VIBRATOR_INTENSITY setting
    Change-Id: I55025639036becd6df6c945abc904a531ffa4207
    TICKET:CYNGNOS-1166

--- src/java/cyanogenmod/providers/CMSettings.java
+++ src/java/cyanogenmod/providers/CMSettings.java
@@ -2660,6 +2660,12 @@ public final class CMSettings {
          */
         public static final String FEATURE_TOUCH_HOVERING = "feature_touch_hovering";
 
+        /**
+         * Vibrator intensity setting for supported devices
+         * @hide
+         */
+        public static final String VIBRATOR_INTENSITY = "vibrator_intensity";
+
         // endregion
 
         /**
