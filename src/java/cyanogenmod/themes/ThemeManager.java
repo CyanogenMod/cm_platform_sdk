2cebb1c23ecfa3b3bc80d579369dff6a8dc9a9bd
Bryson Reece <brysonreece@users.noreply.github.com>
2016-04-01 13:04:20 -0500

    Fixed typo

--- src/java/cyanogenmod/themes/ThemeManager.java
+++ src/java/cyanogenmod/themes/ThemeManager.java
@@ -53,7 +53,7 @@ public class ThemeManager {
         if (context.getPackageManager().hasSystemFeature(
                 CMContextConstants.Features.THEMES) && sService == null) {
             throw new RuntimeException("Unable to get ThemeManagerService. The service either" +
-                    " crashed, was not started, or the interface has been called to early in" +
+                    " crashed, was not started, or the interface has been called too early in" +
                     " SystemServer init");
         }
         mHandler = new Handler(Looper.getMainLooper());
