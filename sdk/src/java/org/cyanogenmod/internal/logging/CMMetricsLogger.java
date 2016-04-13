5e7140a8701734f52264da0d5476822275d75e4e
Keith Mok <kmok@cyngn.com>
2016-04-13 08:50:53 -0700

    cmsdk: Add motion service
    Add a motion service for handling flip to mute incoming
    call
    
    Change-Id: I673766c9146e20461ec2c95c375cbeb5822b2be9

--- sdk/src/java/org/cyanogenmod/internal/logging/CMMetricsLogger.java
+++ sdk/src/java/org/cyanogenmod/internal/logging/CMMetricsLogger.java
@@ -68,4 +68,5 @@ public class CMMetricsLogger extends MetricsLogger {
     public static final int TILE_CAFFEINE = BASE + 41;
     public static final int WEATHER_SETTINGS = BASE + 42;
     public static final int TILE_THEMES = BASE + 43;
+    public static final int MOTION_SETTINGS = BASE + 44;
 }
