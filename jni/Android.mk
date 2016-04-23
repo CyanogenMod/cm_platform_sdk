0837b7d42950cd11dec313797b1d317c70a820d9
Steve Kondik <steve@cyngn.com>
2016-04-23 00:57:07 -0700

    cmsdk: Add AudioSessionObserver API
     * This is a rework of the session callback API which previously
       lived in the framework due to JNI usage. This has been split out
       and cleaned up for CMSDK.
     * The JNI library lives on the client side, however the API has been
       written such that it will fail gracefully if an app was built
       with the SDK and the library is not bundled. There is no need
       to ship this with applications as it serves a similar purpose
       as libmedia_jni does on the platform.
    
    Change-Id: I31c293943474419e3db088bb7ffab75f7440ac0f

---
+++ jni/Android.mk
@@ -0,0 +1,26 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    src/cyanogenmod_media_AudioSessionObserver.cpp \
+    src/onload.cpp
+
+LOCAL_C_INCLUDES += \
+    $(JNI_H_INCLUDE) \
+    $(TOP)/frameworks/base/core/jni \
+    $(TOP)/frameworks/av/include
+
+LOCAL_SHARED_LIBRARIES := \
+    libandroid_runtime \
+    libmedia \
+    liblog \
+    libcutils \
+    libutils \
+    
+LOCAL_MODULE := libcmsdk_media_jni
+LOCAL_MODULE_TAGS := optional
+LOCAL_CFLAGS += -Wall -Werror -Wno-unused-parameter
+
+include $(BUILD_SHARED_LIBRARY)
+
