2fc4284dc958ed3201f792a53e9dcef5dfe26db1
Danny Baumann <dannybaumann@web.de>
2016-04-28 10:06:23 +0200

    Determine default temperature unit based on user's locale.
    Change-Id: Ia1c71ef60744ab851f945ecc60f1a0ebed81a773

--- sdk/src/java/cyanogenmod/weather/CMWeatherManager.java
+++ sdk/src/java/cyanogenmod/weather/CMWeatherManager.java
@@ -33,6 +33,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 
@@ -139,14 +140,10 @@ public class CMWeatherManager {
         }
 
         try {
-            int tempUnit = CMSettings.Global.getInt(mContext.getContentResolver(),
-                    CMSettings.Global.WEATHER_TEMPERATURE_UNIT,
-                        WeatherContract.WeatherColumns.TempUnit.FAHRENHEIT);
-
             RequestInfo info = new RequestInfo
                     .Builder(mRequestInfoListener)
                     .setLocation(location)
-                    .setTemperatureUnit(tempUnit)
+                    .setTemperatureUnit(getSelectedTemperatureUnit(mContext))
                     .build();
             if (listener != null) mWeatherUpdateRequestListeners.put(info, listener);
             sWeatherManagerService.updateWeather(info);
@@ -177,14 +174,10 @@ public class CMWeatherManager {
         }
 
         try {
-            int tempUnit = CMSettings.Global.getInt(mContext.getContentResolver(),
-                    CMSettings.Global.WEATHER_TEMPERATURE_UNIT,
-                        WeatherContract.WeatherColumns.TempUnit.FAHRENHEIT);
-
             RequestInfo info = new RequestInfo
                     .Builder(mRequestInfoListener)
                     .setWeatherLocation(weatherLocation)
-                    .setTemperatureUnit(tempUnit)
+                    .setTemperatureUnit(getSelectedTemperatureUnit(mContext))
                     .build();
             if (listener != null) mWeatherUpdateRequestListeners.put(info, listener);
             sWeatherManagerService.updateWeather(info);
@@ -279,6 +272,28 @@ public class CMWeatherManager {
                 }
             }
         }
+    }
+
+    /**
+     * Gets the currently selected temperature unit.
+     * If none is selected yet, returns a unit appropriate for the current locale
+     *
+     * @hide
+     */
+    public static int getSelectedTemperatureUnit(Context context) {
+        int tempUnit = CMSettings.Global.getInt(context.getContentResolver(),
+                CMSettings.Global.WEATHER_TEMPERATURE_UNIT, -1);
+        if (tempUnit != -1) {
+            return tempUnit;
+        }
+
+        Locale locale = context.getResources().getConfiguration().locale;
+        boolean useFahrenheit = locale.equals(Locale.US)
+                || locale.toString().equals("ms_MY") // Malaysia
+                || locale.toString().equals("si_LK"); // Sri Lanka
+        return useFahrenheit
+                ? WeatherContract.WeatherColumns.TempUnit.FAHRENHEIT
+                : WeatherContract.WeatherColumns.TempUnit.CELSIUS;
     }
 
     /**
