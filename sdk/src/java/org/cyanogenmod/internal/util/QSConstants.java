4ab4193faaaa8c31e7b1f43471375f48909e5947
d34d <clark@cyngn.com>
2016-04-29 16:49:33 -0700

    QS: Make themes a first class citizen again [2/2]
    Change-Id: Icd397a2ddd730ae525233b66288fde9a6798c221
    TICKET: CYNGNOS-2670

--- sdk/src/java/org/cyanogenmod/internal/util/QSConstants.java
+++ sdk/src/java/org/cyanogenmod/internal/util/QSConstants.java
@@ -54,6 +54,7 @@ public class QSConstants {
     public static final String TILE_CAFFEINE = "caffeine";
     public static final String TILE_EDIT = "edit";
     public static final String TILE_DND = "dnd";
+    public static final String TILE_THEMES = "themes";
 
     public static final String DYNAMIC_TILE_NEXT_ALARM = "next_alarm";
     public static final String DYNAMIC_TILE_IME_SELECTOR = "ime_selector";
@@ -99,6 +100,7 @@ public class QSConstants {
         STATIC_TILES_AVAILABLE.add(TILE_SYNC);
         STATIC_TILES_AVAILABLE.add(TILE_BATTERY_SAVER);
         STATIC_TILES_AVAILABLE.add(TILE_CAFFEINE);
+        STATIC_TILES_AVAILABLE.add(TILE_THEMES);
 
         TILES_AVAILABLE.addAll(STATIC_TILES_AVAILABLE);
 
