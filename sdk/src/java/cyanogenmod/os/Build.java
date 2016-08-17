c349280cf8f7754fb0bfe21554427e8a77708326
Adnan Begovic <adnan@cyngn.com>
2016-08-17 11:27:43 -0700

    cmsdk: Finalize api level 6 (Fig).
    Change-Id: I529806e5526faf7d23689354a6cc958d9989b95c

--- sdk/src/java/cyanogenmod/os/Build.java
+++ sdk/src/java/cyanogenmod/os/Build.java
@@ -133,6 +133,7 @@ public class Build {
          *
          * <p>Applications targeting this or a later version will get access to these
          * new features!</p>
+         * <ul>
          * <li>Weather request api to fetch weather data from providers on the device
          * {@link cyanogenmod.weather.CMWeatherManager}</li>
          * <li>Weather provider api to provide weather data to any listener on the device
@@ -151,7 +152,21 @@ public class Build {
         public static final int ELDERBERRY = 5;
 
         /**
-         * Future Release - 3rd quarter 2016
+         * August 2016: The 6th iteration of the platform sdk for CyanogenMod
+         *
+         * <p>Applications targeting this or a later version will get access to these
+         * new features!</p>
+         * <ul>
+         * <li>Ability to query and color balance ranges from the
+         * {@link cyanogenmod.hardware.CMHardwareManager}, as well as do picture adjustment</li>
+         * <li>Extended capabilities of the LiveDisplay interfaces, now providing
+         * {@link cyanogenmod.hardware.LiveDisplayConfig} and a dedicated
+         * {@link cyanogenmod.hardware.LiveDisplayManager}</li>
+         * <li>Added new settings, such as LOCKSCREEN_ROTATION and DISPLAY_LOW_POWER
+         * to {@link cyanogenmod.providers.CMSettings}</li>
+         * </ul>
+         *
+         * Signing out, Adnan \u270C
          */
         public static final int FIG = 6;
     }
