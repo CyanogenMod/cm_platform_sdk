1889703097b544ba1a4f322074d445992f66334f
LorDClockaN <lordclockan@gmail.com>
2016-10-02 13:21:27 +0200

    SettingsProviderTest: DEV_FORCE_SHOW_NAVBAR is in Secure now
    Change-Id: Ie5e89a4cd42f8c765496e6bd7b3e16b93938f9ec

--- packages/CMSettingsProvider/tests/src/org/cyanogenmod/cmsettings/tests/CMSettingsProviderDefaultsTest.java
+++ packages/CMSettingsProvider/tests/src/org/cyanogenmod/cmsettings/tests/CMSettingsProviderDefaultsTest.java
@@ -124,7 +124,7 @@ public class CMSettingsProviderDefaultsTest extends AndroidTestCase {
                 CMSettings.Global.WEATHER_TEMPERATURE_UNIT,
                 "R.integer.def_temperature_unit"));
         GLOBAL_SETTINGS_DEFAULTS.add(new Setting(
-                CMSettings.Global.DEV_FORCE_SHOW_NAVBAR,
+                CMSettings.Secure.DEV_FORCE_SHOW_NAVBAR,
                 "R.integer.def_force_show_navbar"));
     }
 
