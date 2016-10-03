b970b2a3e837aa926286e0348c966f7334efe690
Clark Scheff <clark@scheffsblend.com>
2016-10-03 15:31:30 -0700

    themes: port to cm14 [3/4]
    Change-Id: I9ad52b6a368aa0a67b59789b493df0b16b7b78e9

---
+++ cmds/tm/Android.mk
@@ -0,0 +1,16 @@
+# Copyright 2016 The CyanogenMod Project
+#
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := $(call all-subdir-java-files)
+LOCAL_STATIC_JAVA_LIBRARIES := org.cyanogenmod.platform.internal
+LOCAL_MODULE := tm
+include $(BUILD_JAVA_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := tm
+LOCAL_SRC_FILES := tm
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_PREBUILT)
