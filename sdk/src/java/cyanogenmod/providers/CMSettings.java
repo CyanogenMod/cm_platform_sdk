e721f8cff4b336954ef01c19f38aead0c5e6d775
Steve Kondik <steve@cyngn.com>
2016-10-18 23:37:02 -0700

    cmsdk: Add support for split screen button behavior
    Change-Id: I7289e8abe23d047f909066a9986e8b9d55bec27e

--- sdk/src/java/cyanogenmod/providers/CMSettings.java
+++ sdk/src/java/cyanogenmod/providers/CMSettings.java
@@ -984,12 +984,13 @@ public final class CMSettings {
          * 6 - Launch Camera
          * 7 - Action Sleep
          * 8 - Last app
+         * 9 - Toggle split screen
          */
         public static final String KEY_HOME_LONG_PRESS_ACTION = "key_home_long_press_action";
 
         /** @hide */
         public static final Validator KEY_HOME_LONG_PRESS_ACTION_VALIDATOR =
-                new InclusiveIntegerRangeValidator(0, 8);
+                new InclusiveIntegerRangeValidator(0, 9);
 
         /**
          * Action to perform when the home key is double-tapped.
@@ -1000,7 +1001,7 @@ public final class CMSettings {
 
         /** @hide */
         public static final Validator KEY_HOME_DOUBLE_TAP_ACTION_VALIDATOR =
-                new InclusiveIntegerRangeValidator(0, 8);
+                new InclusiveIntegerRangeValidator(0, 9);
 
         /**
          * Whether to wake the screen with the back key, the value is boolean.
@@ -1040,7 +1041,7 @@ public final class CMSettings {
 
         /** @hide */
         public static final Validator KEY_MENU_ACTION_VALIDATOR =
-                new InclusiveIntegerRangeValidator(0, 8);
+                new InclusiveIntegerRangeValidator(0, 9);
 
         /**
          * Action to perform when the menu key is long-pressed.
@@ -1051,7 +1052,7 @@ public final class CMSettings {
 
         /** @hide */
         public static final Validator KEY_MENU_LONG_PRESS_ACTION_VALIDATOR =
-                new InclusiveIntegerRangeValidator(0, 8);
+                new InclusiveIntegerRangeValidator(0, 9);
 
         /**
          * Action to perform when the assistant (search) key is pressed. (Default is 3)
@@ -1061,7 +1062,7 @@ public final class CMSettings {
 
         /** @hide */
         public static final Validator KEY_ASSIST_ACTION_VALIDATOR =
-                new InclusiveIntegerRangeValidator(0, 8);
+                new InclusiveIntegerRangeValidator(0, 9);
 
         /**
          * Action to perform when the assistant (search) key is long-pressed. (Default is 4)
@@ -1071,7 +1072,7 @@ public final class CMSettings {
 
         /** @hide */
         public static final Validator KEY_ASSIST_LONG_PRESS_ACTION_VALIDATOR =
-                new InclusiveIntegerRangeValidator(0, 8);
+                new InclusiveIntegerRangeValidator(0, 9);
 
         /**
          * Action to perform when the app switch key is pressed. (Default is 2)
@@ -1081,7 +1082,7 @@ public final class CMSettings {
 
         /** @hide */
         public static final Validator KEY_APP_SWITCH_ACTION_VALIDATOR =
-                new InclusiveIntegerRangeValidator(0, 8);
+                new InclusiveIntegerRangeValidator(0, 9);
 
         /**
          * Action to perform when the app switch key is long-pressed. (Default is 0)
@@ -1091,7 +1092,7 @@ public final class CMSettings {
 
         /** @hide */
         public static final Validator KEY_APP_SWITCH_LONG_PRESS_ACTION_VALIDATOR =
-                new InclusiveIntegerRangeValidator(0, 8);
+                new InclusiveIntegerRangeValidator(0, 9);
 
         /**
          * Whether to wake the screen with the home key, the value is boolean.
