da96b0e8a8a70aaf2e527014aafe6b583ab43eda
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-11-17 23:36:25 +0800

    cmsdk: Add CMHW touchscreen gestures support
    Change-Id: Iabeee255748fc92376bc861409a7074de570cb0c

--- sdk/src/java/cyanogenmod/hardware/CMHardwareManager.java
+++ sdk/src/java/cyanogenmod/hardware/CMHardwareManager.java
@@ -169,6 +169,12 @@ public final class CMHardwareManager {
     @VisibleForTesting
     public static final int FEATURE_PICTURE_ADJUSTMENT = 0x40000;
 
+    /**
+     * Touchscreen gesture
+     */
+    @VisibleForTesting
+    public static final int FEATURE_TOUCHSCREEN_GESTURES = 0x80000;
+
     private static final List<Integer> BOOLEAN_FEATURES = Arrays.asList(
         FEATURE_ADAPTIVE_BACKLIGHT,
         FEATURE_COLOR_ENHANCEMENT,
@@ -1025,6 +1031,46 @@ public final class CMHardwareManager {
         try {
             if (checkService()) {
                 return sService.unRegisterThermalListener(thermalCallback);
+            }
+        } catch (RemoteException e) {
+        }
+        return false;
+    }
+
+    /**
+     * @return a list of available touchscreen gestures on the devices
+     */
+    public TouchscreenGesture[] getTouchscreenGestures() {
+        try {
+            if (checkService()) {
+                return sService.getTouchscreenGestures();
+            }
+        } catch (RemoteException e) {
+        }
+        return null;
+    }
+
+    /**
+     * @return true if the touchscreen gesture is enabled
+     */
+    public boolean isTouchscreenGestureEnabled(TouchscreenGesture gesture) {
+        try {
+            if (checkService()) {
+                return sService.isTouchscreenGestureEnabled(gesture);
+            }
+        } catch (RemoteException e) {
+        }
+        return false;
+    }
+
+    /**
+     * @return true if setting the activation status was successful
+     */
+    public boolean setTouchscreenGestureEnabled(
+            TouchscreenGesture gesture, boolean state) {
+        try {
+            if (checkService()) {
+                return sService.setTouchscreenGestureEnabled(gesture, state);
             }
         } catch (RemoteException e) {
         }
