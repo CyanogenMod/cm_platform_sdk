131419c4bf034576da10d4bd57ea6929573a1c66
Michael W <baddaemon87@gmail.com>
2016-12-07 16:20:38 +0100

    utils: Introduce PackageUtils class
    Source: https://github.com/CyanogenMod/android_packages_apps_Launcher3/
    blob/cm-14.1/src/com/android/launcher3/util/PackageManagerHelper.java
    
    Change-Id: I0ed2ea210cb52ac4bd94c7bc5320c24e6d423279

---
+++ sdk/src/java/org/cyanogenmod/internal/util/PackageUtils.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cyanogenmod.internal.util;
+
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+
+public final class PackageUtils {
+    private static final int FLAG_SUSPENDED = 1<<30;
+
+    private PackageUtils() {
+        // This class is not supposed to be instantiated
+    }
+
+    /**
+     * Checks whether a given package exists
+     *
+     * @return true if package exists
+     */
+    public static boolean isAppPresent(final Context context, final String packageName) {
+        final PackageManager packageManager = context.getPackageManager();
+        try {
+            packageManager.getApplicationInfo(packageName, 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Checks whether a given package exists and is enabled
+     *
+     * @return true if package is enabled
+     */
+    public static boolean isAppEnabled(final Context context, final String packageName) {
+        return isAppEnabled(context, packageName, 0);
+    }
+
+    /**
+     * Checks whether a given package with the given flags exists and is enabled
+     *
+     * @return true if package is enabled
+     */
+    public static boolean isAppEnabled(final Context context,
+                                       final String packageName, final int flags) {
+        final PackageManager packageManager = context.getPackageManager();
+        try {
+            final ApplicationInfo info = packageManager.getApplicationInfo(packageName, flags);
+            return info != null && info.enabled;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Checks if the app can possibly be on the SDCard. This is just a workaround and doesn't
+     * guarantee that the app is on SD card.
+     *
+     * @return true if app is on SDCard
+     */
+    public static boolean isAppOnSdcard(final Context context, final String packageName) {
+        return isAppEnabled(context, packageName, PackageManager.GET_UNINSTALLED_PACKAGES);
+    }
+
+    /**
+     * Checks if the app is suspended
+     *
+     * @return true if the app is suspended
+     */
+    public static boolean isAppSuspended(Context context, String packageName) {
+        try {
+            final PackageManager packageManager = context.getPackageManager();
+            final ApplicationInfo info = packageManager.getApplicationInfo(packageName, 0);
+            return info != null && isAppSuspended(info);
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Checks if the app is suspended
+     *
+     * @return true if the app is suspended
+     */
+    public static boolean isAppSuspended(final ApplicationInfo info) {
+        return (info != null) && ((info.flags & FLAG_SUSPENDED) != 0);
+    }
+}
